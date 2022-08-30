4ede58602df985ddca8b8fe2066c6d2862bee6ec
Lin Tang <1508014225@qq.com>
2022-08-30 23:37:43 +0800

    修改了登录拦截

--- gateway/src/main/java/cn/yuanerya/gateway/AuthorizeFilter.java
+++ gateway/src/main/java/cn/yuanerya/gateway/AuthorizeFilter.java
@@ -2,22 +2,21 @@ package cn.yuanerya.gateway;
 
 
 import io.jsonwebtoken.Jwts;
-import net.sf.json.JSONObject;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.io.buffer.DataBuffer;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
+
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
+
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
